6a34c053b5a6d8782b4f9b59633a9f1b371a8d3f
Volodymyr Minchenko <voldemarprk2001@gmail.com>
2021-12-29 12:19:49 +0200

    Initialize data security system module

---
+++ data-security-system/src/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+  const config = new DocumentBuilder()
+    .setTitle('My some project')
+    .setDescription('The project API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+bootstrap();
